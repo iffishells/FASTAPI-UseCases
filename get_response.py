ab8c2a5ca17cda9ea0f42904c80289b237b7c4c7
iffishells <iffishells@gmail.com>
2024-08-21 13:33:07 +0500

    1.Added example Redis Queue

--- get_response.py
+++
@@ -1,4 +0,0 @@
-import requests
-task_id = "f4e55b6a-9041-4b13-9fe8-4c8edccc0097'"
-response = requests.get(f"http://127.0.0.1:8000/task/{task_id}")
-print(response.json())
--- tasks.py
+++
@@ -1,6 +0,0 @@
-# tasks.py
-import time
-
-def send_email(email: str):
-    time.sleep(10)  # Simulate a delay (e.g., sending an email)
-    return f"Email sent to {email}"
